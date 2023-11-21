826de217c66d8944c7990387768ccb804ee0e0b9
Pajsen9263 <42203569+Pajsen9263@users.noreply.github.com>
2023-11-21 11:10:05 +0100

    Changed to EJS, added low res images and turned to a more dynamic rendering and listning

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 // index.js
 const express = require('express');
 const path = require('path');
-
+const ejs = require("ejs");
 const app = express();
 const port = 3000;
 
 // Set up static file serving from the 'Static' folder
 app.use('/', express.static(path.join(__dirname, 'static')));
-
+app.set('view engine', 'ejs');
 
 
 // Define a route to serve the index.html file
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+    //res.sendFile(path.join(__dirname, 'views', 'index.html'));
+    const trainsObject = require('./images.json');
+  
+    // Convert the object keys to an array
+    const trainsArray = Object.values(trainsObject);    
+    res.render(path.join(__dirname, 'views', 'index.ejs'), {  trains: trainsArray }); // Renders the 'index.ejs' file with the 'trains' data
 });
 
 // Start the server
